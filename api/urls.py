9c43ec3da192db147d7202d9400272cc23da3739
dmitrij-rmshv <gdems@fastmail.com>
2022-12-11 18:39:40 +0300

    APIended

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import bbs
+from .views import bbs, BbDetailView, comments
 
 urlpatterns = [
+    path('bbs/<int:pk>/comments', BbDetailView.as_view()),
+    path('bbs/<int:pk>/', comments),
     path('bbs/', bbs),
 ]
